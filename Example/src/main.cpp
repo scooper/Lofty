f4eab2fbd65aa649c56db165b65e0e08e3e4949d
Sam <samuel.j.cooper@outlook.com>
2021-01-15 17:21:18 +0000

    Added basic skeleton project using cmake - as a lib being linked into an executable

---
+++ Example/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <Core/App.h>
+
+int main()
+{
+    std::cout << "Hello World! This is Lofty Sample and I compile with Lofty linked with me!" << std::endl;
+    return 0;
+}
